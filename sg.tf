09fb84484da205fc4108808a82057e9fa242e8aa
spandanakoppaku <skoppaku@gmail.com>
2023-08-10 11:05:55 -0400

    Auto Push

--- sg.tf
+++ sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "alb_public" {
     description = "http from internet"
     from_port   = 80
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
---
+++ vars.tf
@@ -0,0 +1,2 @@
+variable "ENV"{}
+variable "ALB_NAME"{}
