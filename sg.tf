3922719780bb775e49d6c8ef74622715f7e9d456
spandanakoppaku <skoppaku@gmail.com>
2023-08-10 11:10:43 -0400

    Auto Push

--- sg.tf
+++ sg.tf
@@ -1,6 +1,8 @@
+
+# Public load balancer security group
 resource "aws_security_group" "alb_public" {
   name        = "roboshop-${var.ENV}-public-alb-sg"
-  description = "allows traffic from internal"
+  description = "Allows traffic from internal"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
@@ -9,6 +11,33 @@ resource "aws_security_group" "alb_public" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-alb-sg"
+  }
+}
+
+
+# Private load balancer security group
+resource "aws_security_group" "alb_private" {
+  name        = "roboshop-${var.ENV}-private-alb-sg"
+  description = "Allows traffic from intranet"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "http from intranet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
 
   egress {
